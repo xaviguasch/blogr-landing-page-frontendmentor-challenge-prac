282a85b661c3f1088631d5254372cca6fee12b99
xaviguasch <xaviguasch@gmail.com>
2021-08-03 10:35:55 +0200

    finished modal navigation for mobile, drop menu js functionality included

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+// Selectors
 const openNav = document.querySelector('.open-nav')
 const closeNav = document.querySelector('.close-nav')
 const nav = document.querySelector('.nav')
 
+const navLinks = document.querySelectorAll('.nav-link--main')
+
+// Functions
 const showModal = () => {
   openNav.style.display = 'none'
   closeNav.style.display = 'block'
@@ -16,5 +20,18 @@ const hideModal = () => {
   nav.classList.remove('open')
 }
 
+const showDrop = (e) => {
+  navLinks.forEach((navLink) => {
+    navLink.classList.remove('open')
+  })
+
+  e.target.parentElement.classList.add('open')
+}
+
+// Event listeners
 openNav.addEventListener('click', showModal)
 closeNav.addEventListener('click', hideModal)
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener('click', showDrop)
+})
